6b2eecaadc347dcc7cc5901bd232c7054df070cc
Evelin <evelin@gluonixtech.com>
2019-05-24 21:00:32 -0500

    Olympia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeControllerComponent } from './home-controller/home-controller.component';
+import { LanguageBarComponent } from './language-bar/language-bar.component';
+import { SnippetCardComponent } from './snippet-card/snippet-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeControllerComponent,
+    LanguageBarComponent,
+    SnippetCardComponent
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/home-controller/home-controller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-controller',
+  templateUrl: './home-controller.component.html',
+  styleUrls: ['./home-controller.component.scss']
+})
+export class HomeControllerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
